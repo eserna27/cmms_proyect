becb93a116e55eb9c3adafec273c589c04f71b74
emmanuel <eserna27@gmail.com>
2014-11-04 12:09:43 -0600

    Use SSL and Unicorn in production

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,17 +3,17 @@ timeout 15
 preload_app true
 
 before_fork do |server, worker|
-	Signal.trap 'TERM' do
+  Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.conection.disconnect!
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
-    msg = 'Unicorn worker intercepting TERM and doing nothing. '
+    msg  = 'Unicorn worker intercepting TERM and doing nothing. '
     msg += 'Wait for master to send QUIT'
     puts msg
   end
