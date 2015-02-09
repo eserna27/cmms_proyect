cf7e582eaa91bd1c56299f569abc65565b9e4c5f
emmanuel <eserna27@gmail.com>
2015-02-09 17:59:04 -0600

    cambio correo

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "eserna27@gmail.com"
+  default from: "contacto@jazzhealthcare.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
